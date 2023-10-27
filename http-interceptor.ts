431aa419f69ea38a9cfc522e549474b9fd82823f
Ricardo Saracino <ricardosaracino@protonmail.com>
2023-10-27 15:31:07 -0400

    Update http-interceptor.ts

--- http-interceptor.ts
+++ http-interceptor.ts
@@ -4,32 +4,43 @@ export class HttpInterceptorService implements HttpInterceptor {
 
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    if (request.body && !(request.body instanceof FormData)) {
-
-      const body: { [key: string]: any } = {};
-
+if (request.body && !(request.body instanceof FormData)) {
+      
       const isString = (value: any): boolean => typeof value === 'string' || value instanceof String;
 
-      const trimObject = (reqBody: any, _body: any): void => Object.keys(reqBody).forEach(key => {
+      const trimObject = (reqBody: any): any => {
 
-        if (isString(reqBody[key])) {
-          _body[key] = reqBody[key].trim();
-        } else if (reqBody[key] instanceof Array) {
-          _body[key] = [];
-          trimObject(reqBody[key], _body[key]);
-        } else if (reqBody[key] instanceof Object) {
-          _body[key] = {};
-          trimObject(reqBody[key], _body[key]);
-        } else {
-          _body[key] = reqBody[key];
+        if (isString(reqBody)) {
+
+          return reqBody.trim();
+
+        } else if (reqBody instanceof Array) {
+
+          const body = [];
+
+          reqBody.forEach(d =>body.push(trimObject(d)));
+
+          return body;
+
+        } else if (reqBody instanceof Object) {
+          
+          const  body = {};
+
+          Object.keys(reqBody).forEach(key => {
+            body[key] = trimObject(reqBody[key]);
+          });
+
+          return body
         }
-      });
 
-      trimObject(request.body, body);
+        return reqBody;
+      };
+
+      const body = trimObject(request.body);
 
       request = request.clone({ body });
     }
-
+    
     return next.handle(request).pipe(
       map((resp: any) => {
         return resp;
